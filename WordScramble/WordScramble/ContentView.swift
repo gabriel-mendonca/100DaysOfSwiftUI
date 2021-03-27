4b7b0ca6f518141658caef39b3843ffa64f1f67a
gabriel-mendonca <gabrielcruzerence9993@gmail.com>
2021-03-27 15:15:53 -0300

    Day 31

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var punctuation = 0
+    @State private var maxScore = 0
     
     @State private var errorTitle = ""
     @State private var errorMessage = ""
@@ -27,9 +29,16 @@ struct ContentView: View {
                     Image(systemName: "\($0.count).circle")
                     Text($0)
                 }
+                Text("Score: \(punctuation)")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
             }
             .navigationBarTitle(rootWord)
             .onAppear(perform: startGame)
+            .navigationBarItems(leading: Button("New game", action: {
+                startGame()
+                usedWords.removeAll()
+            }),trailing: Text("max. Score: \(maxScore)").font(.title3).fontWeight(.semibold))
             .alert(isPresented: $showingError, content: {
                 Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
             })
@@ -41,6 +50,8 @@ struct ContentView: View {
             if let startWord = try? String(contentsOf: startWordsURL) {
                 let allwords = startWord.components(separatedBy: "\n")
                 rootWord = allwords.randomElement() ?? "silkworm"
+                punctuation = 0
+                newWord = ""
                 return
         }
     }
@@ -65,6 +76,8 @@ struct ContentView: View {
         }
         
         usedWords.insert(answer, at: 0)
+        punctuation += 2
+        maxScore += 2
         newWord = ""
     }
     
@@ -77,6 +90,7 @@ struct ContentView: View {
         
         for letter in word {
             if let pos = tempWord.firstIndex(of: letter) {
+                if tempWord == word { return false }
                 tempWord.remove(at: pos)
             } else {
                 return false
@@ -88,6 +102,7 @@ struct ContentView: View {
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
+        if word.utf16.count < 3 { return false }
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         return misspelledRange.location == NSNotFound
     }
