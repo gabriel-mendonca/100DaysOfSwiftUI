337a7ac29a4ee1a03c1fd0a2d9b2708fb79754a6
gabriel-mendonca <gabrielcruzerence9993@gmail.com>
2021-04-07 00:51:43 -0300

    Day 40

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Gabriel Mendonça Sousa Gonçalves  on 06/04/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    var id: String
+    var name: String
+    var description: String
+}
---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Gabriel Mendonça Sousa Gonçalves  on 05/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail View")) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                    
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }.navigationBarTitle("Moonshot")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
