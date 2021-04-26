5367048b8edf3f434db0a752a94a1e736525f145
gabriel-mendonca <gabrielcruzerence9993@gmail.com>
2021-04-26 07:59:53 -0300

    Day 57

---
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Gabriel Mendonça Sousa Gonçalves  on 25/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    
+    var body: some View {
+        VStack {
+            List(wizards, id: \.self) { wizard in
+                Text(wizard.name ?? "Unknown")
+            }
+            
+            Button("Add") {
+                let wizard = Wizard(context: moc)
+                wizard.name = "Harry Potter"
+            }
+            
+            Button("Save") {
+                do {
+                    try self.moc.save()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Gabriel Mendonça Sousa Gonçalves  on 25/04/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
